648bfcd9a80190905aefac6aabdb756b7a4fee22
cmeo226 <cmeo226@yahoo.com>
2013-08-12 22:35:36 +0000

    Added reposistories for the emacs packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,7 +4,11 @@
 ;; ---------------------
 ;; -- Global Settings --
 ;; ---------------------
+
 (add-to-list 'load-path "~/.emacs.d")
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 (require 'cl)
 (require 'ido)
 (require 'ffap)
